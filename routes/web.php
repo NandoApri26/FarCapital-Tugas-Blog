561ce6af86d4833f8e6905fe6a577abe37753fd8
NandoApri26 <apriadno.pratamaa@gmail.com>
2022-12-13 10:13:56 +0700

    create route index article

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ArticleController;
 use App\Models\Product;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +24,15 @@ use App\Models\Product;
 
 Route::get('/Home', [HomeController::class, 'index']);
 
+
+// Route Product
 Route::get('/Product', [ProductController::class, 'index']);
 Route::get('/Product/create', [ProductController::class, 'create']);
 Route::post('/Product', [ProductController::class, 'store']);
 Route::delete('/Product/{product}', [ProductController::class, 'destroy']); //Fungsi Delete
 Route::get('/Product/{product}/edit', [ProductController::class, 'edit']);
-route::get('/Product/{product}/detail', [ProductController::class, 'show']);
-route::patch('/Product/{product}', [ProductController::class, 'update']);
+Route::get('/Product/{product}/detail', [ProductController::class, 'show']);
+Route::patch('/Product/{product}', [ProductController::class, 'update']);
 
+// Route Article
+Route::get('/Article', [ArticleController::class, 'index']);
